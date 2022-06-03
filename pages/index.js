12b683424d6ffeae72cb33d1b2143e78c4d335ac
yingzhuo <yingzhuo1994@gmail.com>
2022-06-02 23:23:36 -0500

    Add New Meetup Form

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Layout from "../components/layout/Layout";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b2/Skyscrapers_of_Shinjuku_2009_January.jpg",
+    address: "Some address 5, 123456 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b2/Skyscrapers_of_Shinjuku_2009_January.jpg",
+    address: "Some address 10, 123456 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+function HomePage() {
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
+}
+
+export default HomePage;
