8526d302c50f2ebb40c0d27b0186c02aaab6596d
yingzhuo <yingzhuo1994@gmail.com>
2022-06-02 23:52:39 -0500

    Add Custom Components & CSS Module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/b/b2/Skyscrapers_of_Shinjuku_2009_January.jpg"
+      title="A First Meetup"
+      address="Some address 5, 123456 Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
