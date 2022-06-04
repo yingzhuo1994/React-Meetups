67d302c59146c64ec7e560226083f2e59b051026
yingzhuo <yingzhuo1994@gmail.com>
2022-06-03 23:48:02 -0500

    Send Http Requests to API Routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b2/Skyscrapers_of_Shinjuku_2009_January.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some address 5, 123456 Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// post /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+      const data = req.body;
+
+      const client = await MongoClient.connect('mongodb+srv://ying:bbOUshWUFVPklVvx@cluster0.vgp4n.mongodb.net/meetups?retryWrites=true&w=majority');
+      const db = client.db();
+
+      const meetupsCollection = db.collection('meetups');
+
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+
+      client.close();
+
+      res.status(201).json({message: 'Meetup inserted!'});
+  }
+}
+
+export default handler;
