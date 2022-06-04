76485e0e6b18fb77801e425b171ea71586fc76b8
yingzhuo <yingzhuo1994@gmail.com>
2022-06-04 13:13:38 -0500

    Add "head" Metadata

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,23 @@
 import { MongoClient, ObjectId } from "mongodb";
+import { Fragment } from "react";
+import Head from "next/head";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image={props.meetupData.image}
-      title={props.meetupData.title}
-      address={props.meetupData.address}
-      description={props.meetupData.description}
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
 }
 
@@ -21,6 +29,7 @@ export async function getStaticPaths() {
 
   const meetupsCollection = db.collection("meetups");
 
+  // Only fetch the Ids of objects
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
 
   client.close();
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,6 +1,8 @@
 // our-domain.com/new-meetup
 
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
@@ -8,21 +10,32 @@ function NewMeetupPage() {
   const router = useRouter();
 
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('/api/new-meetup', {
-      method: 'POST',
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
       body: JSON.stringify(enteredMeetupData),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     const data = await response.json();
 
     console.log(data);
 
-    router.push('/');
+    router.push("/");
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add our own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
